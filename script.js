68393adc66ef16c4873aeca07e635e5bba67aac6
JoseSalinasG <149660068+JoseSalinasG@users.noreply.github.com>
2023-11-22 23:18:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const obtenerHeader = document.querySelector('header');
+const obtenerButton = document.querySelector('button');
+const obtenerTarjeta = document.querySelector('.tarjeta');
+const obtenerFooter = document.querySelector('footer');
